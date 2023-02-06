d79ad669991fed21935fb1e8597110caae83a731
Kenny Li <jasonqwert99@gmail.com>
2023-02-06 01:04:44 -0500

    UART commmunication

--- Python_script/motor.py
+++ Python_script/motor.py
@@ -1,58 +1,58 @@
-import serial  # 导入串口包
-import re
+import serial as ser # 导入串口包
+
 try:
-    ser = serial.Serial("/dev/ttyACM1",115200,timeout = 5)  # 开启com3口，波特率115200，超时5
-    ser.flushInput()  # 清空缓冲区
+    se = ser.Serial("/dev/ttyACM1",115200,timeout = 5)  # 开启com3口，波特率115200，超时5
+    se.flushInput()  # 清空缓冲区
     print("serial open success!")
 except:
     print("serial open fail!")
 ########################打包函数######################################   
-class Motor_control:
-    def speed_control(motor_id, speed):
+class Motor_control(object):
+    def __init__(self, motorID):
+        self.motorID = motorID
+
+
+    def speed_control(self, motorSpeed):
         data = [0,0,0,0]
-        data[0] = motor_id
-        data[1] = speed
+        data[0] = self.motorID
+        data[1] = motorSpeed * 100
         for i in range(2):
-            Motor_uart.senddata(1,data)
-        print("speed_control-->motor_id=%d,speed=%d"%(motor_id,speed))
+            senddata(1,data)
+        print("speed_control-->motor_id=%d,speed=%d"%(self.motorID,motorSpeed))
+    
+    # def angle_control(self, motorAngle):
+
         
 
 ########################打包函数######################################  
-class Motor_uart:
-    def senddata(id,data):
-        data_end=[0,0,0,0]
-        if id <10 :
-            id_end = "0"+str(id)
-        else:
-            id_end = str(id)
+def to_2d_array(num):
+    num_str = str(num).zfill(5)
+    return [[int(num_str[i + j]) for i in range(0, 5, 5)] for j in range(5)]
 
-        for i in range(4):
+def senddata(id,data):
+    data_end=[0,0,0,0]
+    if id <10 :
+        id_end = "0"+str(id)
+    else:
+        id_end = str(id)
 
-            if data[i]<10:
-                data_end[i] = "0000"+str(data[i])
-            elif data[i]<100:
-                data_end[i] = "000"+str(data[i])
-            elif data[i]<1000:
-                data_end[i] = "00"+str(data[i])
-            elif data[i]<10000:
-                data_end[i] = "0"+str(data[i])
-            else:
-                data_end[i] = str(data[i])  
-                
-            # print("data_end:", i,data_end[i])    
-        ser.write((id_end+str(data_end[0])+str(data_end[1])+str(data_end[2])+str(data_end[3])+"*/").encode('ascii')) 
+    memory = to_2d_array(data[0])+to_2d_array(data[1])+to_2d_array(data[2])+to_2d_array(data[3])
+    to_1d_array = sum(memory, [])
+    to_str = ''.join([str(x) for x in to_1d_array])
+        # print("data_end:", i,data_end[i])    
+    # se.write((id_end+str(data_end[0])+str(data_end[1])+str(data_end[2])+str(data_end[3])+"*/").encode('ascii')) 
 
-    def get_data(data):
-        ptr = ['null','temp','iq','speed','encoder','']
-        data = str(data, encoding="'ISO-8859-1'")
-        change_data = data[data.find('outputstate.temperature'):data.find('outputstate_end')]
-        print ("change_data--->",change_data)
+def get_data(data):
+    ptr = ['null','temp','iq','speed','encoder','']
+    data = str(data, encoding="'ISO-8859-1'")
+    change_data = data[data.find('outputstate.temperature'):data.find('outputstate_end')]
+    print ("change_data--->",change_data)
 
-        if 'outputstate' in change_data:
-                line1 = str(change_data)
-                ouput_data = line1.split('outputstate.')
-                for i in range(5):
-                    print("outputstate%d-->"%i, ouput_data[i])
-                    ouput_data[i] = re.sub("\D", "", ouput_data[i])
-                    print("outputstate.%s-->"%ptr[i],ouput_data[i])
-                return ouput_data
+    if 'outputstate' in change_data:
+            line1 = str(change_data)
+            ouput_data = line1.split('outputstate.')
+            for i in range(5):
+                print("outputstate%d-->"%i, ouput_data[i])
+                ouput_data[i] = re.sub("\D", "", ouput_data[i])
+                print("outputstate.%s-->"%ptr[i],ouput_data[i])
+            return ouput_data
--- USER/main.c
+++ USER/main.c
@@ -9,14 +9,14 @@
 #include "timer.h"
 #include "motor_control.h"
 /************************************************
-User manual£º
+User manualï¿½ï¿½
 CAN1 connet to the motor, open serial port, take a look if there is any message print out,Serial port baud rate 115200
 
-motor control command: below is a example for motor speed contro,data=2000£¬
+motor control command: below is a example for motor speed contro,data=2000ï¿½ï¿½
 01000010200000000000*/
 
 /*************************************************/
-#define TIME3_COUNT	    30 //¶¨Ê±Æ÷¼ÆÊý15ms£¬ÓÃÓÚ»ñÈ¡±àÂëÆ÷³õÊ¼encoder
+#define TIME3_COUNT	    30 //ï¿½ï¿½Ê±ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½15msï¿½ï¿½ï¿½ï¿½ï¿½Ú»ï¿½È¡ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ê¼encoder
 
 int main(void)
 {
@@ -32,31 +32,31 @@ int main(void)
 	u8 Get_STDid;
 	u8 *str;
 	motor_state getstate;
-    HAL_Init();                   	//³õÊ¼»¯HAL¿â    
-    Stm32_Clock_Init(336,8,2,7);  	//ÉèÖÃÊ±ÖÓ,168Mhz
-	delay_init(168);               	//³õÊ¼»¯ÑÓÊ±º¯Êý
-	uart_init(115200);             	//³õÊ¼»¯USART
+    HAL_Init();                   	//ï¿½ï¿½Ê¼ï¿½ï¿½HALï¿½ï¿½    
+    Stm32_Clock_Init(336,8,2,7);  	//ï¿½ï¿½ï¿½ï¿½Ê±ï¿½ï¿½,168Mhz
+	delay_init(168);               	//ï¿½ï¿½Ê¼ï¿½ï¿½ï¿½ï¿½Ê±ï¿½ï¿½ï¿½ï¿½
+	uart_init(115200);             	//ï¿½ï¿½Ê¼ï¿½ï¿½USART
 	
-	LED_Init();						//³õÊ¼»¯LED	
-	KEY_Init();						//³õÊ¼»¯KEY
-//   TIM3_Init(TIME3_COUNT-1,8400-1);       //¶¨Ê±Æ÷3³õÊ¼»¯£¬¶¨Ê±Æ÷Ê±ÖÓÎª84M£¬·ÖÆµÏµÊýÎª8400-1£¬
-                                    //ËùÒÔ¶¨Ê±Æ÷3µÄÆµÂÊÎª84M/8400=10K-->0.01ms£¬×Ô¶¯ÖØ×°ÔØÎª5000-1£¬ÄÇÃ´¶¨Ê±Æ÷ÖÜÆÚ¾ÍÊÇ500ms-->5000*0.01ms,´ËÊ±µÄ5000==TIME3_COUNT
+	LED_Init();						//ï¿½ï¿½Ê¼ï¿½ï¿½LED	
+	KEY_Init();						//ï¿½ï¿½Ê¼ï¿½ï¿½KEY
+//   TIM3_Init(TIME3_COUNT-1,8400-1);       //ï¿½ï¿½Ê±ï¿½ï¿½3ï¿½ï¿½Ê¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ê±ï¿½ï¿½Ê±ï¿½ï¿½Îª84Mï¿½ï¿½ï¿½ï¿½ÆµÏµï¿½ï¿½Îª8400-1ï¿½ï¿½
+                                    //ï¿½ï¿½ï¿½Ô¶ï¿½Ê±ï¿½ï¿½3ï¿½ï¿½Æµï¿½ï¿½Îª84M/8400=10K-->0.01msï¿½ï¿½ï¿½Ô¶ï¿½ï¿½ï¿½×°ï¿½ï¿½Îª5000-1ï¿½ï¿½ï¿½ï¿½Ã´ï¿½ï¿½Ê±ï¿½ï¿½ï¿½ï¿½ï¿½Ú¾ï¿½ï¿½ï¿½500ms-->5000*0.01ms,ï¿½ï¿½Ê±ï¿½ï¿½5000==TIME3_COUNT
 	
- 	CAN1_Mode_Init(CAN_SJW_1TQ,CAN_BS2_3TQ,CAN_BS1_3TQ,6,CAN_MODE_NORMAL); //CAN³õÊ¼»¯,²¨ÌØÂÊ1000Kbps     
+ 	CAN1_Mode_Init(CAN_SJW_1TQ,CAN_BS2_3TQ,CAN_BS1_3TQ,6,CAN_MODE_NORMAL); //CANï¿½ï¿½Ê¼ï¿½ï¿½,ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½1000Kbps     
 	CAN_Config();
 	memset(str,0,len);	
     while(1)
     {
        if(USART_RX_STA&0x8000)
 		{					   
-			len=USART_RX_STA&0x3fff;//µÃµ½´Ë´Î½ÓÊÕµ½µÄÊý¾Ý³¤¶È
+			len=USART_RX_STA&0x3fff;//ï¿½Ãµï¿½ï¿½Ë´Î½ï¿½ï¿½Õµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ý³ï¿½ï¿½ï¿½
 			printf("\r\nyou send message len:%d,data:\r\n",len);
 			memcpy(canbuf,USART_RX_BUF,len);
-			HAL_UART_Transmit(&UART1_Handler,(uint8_t*)USART_RX_BUF,len,1000);	//·¢ËÍ½ÓÊÕµ½µÄÊý¾Ý
-			while(__HAL_UART_GET_FLAG(&UART1_Handler,UART_FLAG_TC)!=SET);		//µÈ´ý·¢ËÍ½áÊø
-			printf("\r\n\r\n");//²åÈë»»ÐÐ
+			HAL_UART_Transmit(&UART1_Handler,(uint8_t*)USART_RX_BUF,len,1000);	//ï¿½ï¿½ï¿½Í½ï¿½ï¿½Õµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+			while(__HAL_UART_GET_FLAG(&UART1_Handler,UART_FLAG_TC)!=SET);		//ï¿½È´ï¿½ï¿½ï¿½ï¿½Í½ï¿½ï¿½ï¿½
+			printf("\r\n\r\n");//ï¿½ï¿½ï¿½ë»»ï¿½ï¿½
 			USART_RX_STA=0;
-			//´«Èëµç»ú¿ØÖÆº¯Êý
+			//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æºï¿½ï¿½ï¿½
 			Motor_open_fanction_uart(canbuf,len);
 			printf("%s\r\n",canbuf);
 			for(i=0;i<len;i++)
@@ -68,7 +68,7 @@ int main(void)
 		{
 			times++;
 			if(times%200==0)printf("\r\nplease send data use */ end \r\n");  
-			if(times%30==0)LED0=!LED0;//ÉÁË¸LED,ÌáÊ¾ÏµÍ³ÕýÔÚÔË
+			if(times%30==0)LED0=!LED0;//ï¿½ï¿½Ë¸LED,ï¿½ï¿½Ê¾ÏµÍ³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 			delay_ms(10);   
 		} 
 		} 	
